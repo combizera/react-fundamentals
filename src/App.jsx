4f7ed220af8a35b190740fe171ee660cf2efaa5a
combizera <ycombi349@gmail.com>
2025-04-14 08:32:07 -0300

    feat: finish sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 import { Post } from "./Post"
+
+import styles from './App.module.css'
+
+import "./global.css"
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="John Doe"
-        date="2023-10-01"
-      />
-      <Post
-        author="Jane Smith"
-        date="2023-10-02"
-      />
+
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+
+        <main>
+          <Post
+            author="John Doe"
+            date="2023-10-01"
+          />
+          <Post
+            author="Jane Smith"
+            date="2023-10-02"
+          />
+        </main>
+      </div>
     </>
   )
 }
